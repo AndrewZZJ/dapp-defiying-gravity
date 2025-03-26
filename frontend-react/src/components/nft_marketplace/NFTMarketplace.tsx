179f7e5d016b80df83ddd49f2c83b144f9f5b46f
ParsaBan <mohebanp@yahoo.com>
2025-03-26 13:18:45 -0700

    Feature: Sample NFTs reworked

--- frontend-react/src/components/nft_marketplace/NFTMarketplace.tsx
+++ frontend-react/src/components/nft_marketplace/NFTMarketplace.tsx
@@ -10,8 +10,8 @@ export default function NFTMarketplace() {
       image: "https://cdn.builder.io/api/v1/image/assets/TEMP/804793bedaabda1cf9c4091b86cae6469cbe02c2",
       title: "Fire NFT",
       category: "Mythical",
-      description: "A rare NFT of the elemental water guardian",
-      altText: "Water Spirit",
+      description: "Harness the eternal fire of the sun",
+      altText: "Solar Flame",
     },
     {
       image: "https://cdn.builder.io/api/v1/image/assets/TEMP/da420caf50e77f5a82cd88b049ac8d85fefa8be4",
@@ -22,10 +22,10 @@ export default function NFTMarketplace() {
     },
     {
       image: "https://cdn.builder.io/api/v1/image/assets/TEMP/99c25429344972846f2eaa13689909934b39fdbc",
-      title: "Fire NFT",
+      title: "Flood NFT",
       category: "Epic",
-      description: "Harness the eternal fire of the sun",
-      altText: "Solar Flame",
+      description: "A rare NFT of the elemental water guardian",
+      altText: "Water Spirit",
     },
   ];
 
