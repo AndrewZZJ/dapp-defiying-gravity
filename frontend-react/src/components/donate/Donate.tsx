e86573b80b0b0f862c760c00d0ef3c05be51167f
Parsa Moheban <mohebanp@yahoo.com>
2025-04-05 17:25:22 -0700

    Fix: broken backgrounds on donate and exchange

--- frontend-react/src/components/donate/Donate.tsx
+++ frontend-react/src/components/donate/Donate.tsx
@@ -12,7 +12,7 @@ function Donate() {
         <img
           src="https://cdn.builder.io/api/v1/image/assets/TEMP/1616a3702a56422d49c62b0640e3906f852114ab?placeholderIfAbsent=true&apiKey=a1e026a667a24fc2bd5beacbd7180351"
           alt="Background"
-          className="object-cover absolute inset-0 size-full"
+          className="absolute inset-0 w-full h-full object-cover"
         />
 
         <h1 className="relative text-7xl font-bold tracking-tighter leading-tight text-center whitespace-nowrap text-neutral-950 max-md:text-4xl">
--- frontend-react/src/components/exchange/Exchange.tsx
+++ frontend-react/src/components/exchange/Exchange.tsx
@@ -5,14 +5,14 @@ import { SwapForm } from "./SwapForm";
 
 const Exchange = () => {
   return (
-    <main className="overflow-hidden bg-zinc-800 min-h-screen">
+    <main className="overflow-hidden bg-zinc-800 min-h-screen relative">
       <NavigationHeader />
 
       <section className="relative flex flex-col items-center justify-center px-6 pt-20 pb-40 w-full min-h-[782px] max-md:px-4">
         <img
           src="https://cdn.builder.io/api/v1/image/assets/TEMP/1616a3702a56422d49c62b0640e3906f852114ab?placeholderIfAbsent=true&apiKey=a1e026a667a24fc2bd5beacbd7180351"
           alt="Background"
-          className="object-cover absolute inset-0 size-full"
+          className="absolute inset-0 w-full h-full object-cover"
         />
 
         <h1 className="relative text-7xl font-bold tracking-tighter leading-tight text-center text-neutral-950 max-md:text-4xl mb-10">
